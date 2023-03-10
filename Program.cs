5435e36ada2c8eea3fefaf537f9abc491ab866fd
kaci <kaci.school.info@gmail.com>
2023-03-10 10:42:00 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bank_heist2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
